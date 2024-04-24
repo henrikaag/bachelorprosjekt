5e704e08cd78d07592f4d6f42f31bc4d2c437127
henrikaag <aaghenrik@gmail.com>
2024-04-24 16:51:26 +0200

    links

--- src/components/NavBar/HorizontalNavBar.js
+++ src/components/NavBar/HorizontalNavBar.js
@@ -9,26 +9,26 @@ import '../../css/NavBar/HorizontalNavBar.css';
 function TabsExample() {
   return (
     <Container className='NavigationTabs mt-4'>
-    <Tab.Container id="list-group-tabs-example" defaultActiveKey="#link1">
+    <Tab.Container id="list-group-tabs-example" defaultActiveKey="#forbruk">
       <Row>
         <Col sm={12}>
           <ListGroup horizontal>
-            <ListGroup.Item action href="#link1">
+            <ListGroup.Item action href="#forbruk">
               Forbruk
             </ListGroup.Item>
-            <ListGroup.Item action href="#link2">
+            <ListGroup.Item action href="#fordeg">
               For Deg
             </ListGroup.Item>
-            <ListGroup.Item action href="#link3">
-              Innblikk
+            <ListGroup.Item action href="#innblikk">
+              Innblik
             </ListGroup.Item>
           </ListGroup>
         </Col>
         <Col sm={12}>
           <Tab.Content>
-            <Tab.Pane eventKey="#link1">FORBRUK</Tab.Pane>
-            <Tab.Pane eventKey="#link2">FOR DEG</Tab.Pane>
-            <Tab.Pane eventKey="#link3">INNBLIKK</Tab.Pane>
+            <Tab.Pane eventKey="#forbruk">FORBRUK</Tab.Pane>
+            <Tab.Pane eventKey="#fordeg">FOR DEG</Tab.Pane>
+            <Tab.Pane eventKey="#innblikk">INNBLIKK</Tab.Pane>
           </Tab.Content>
         </Col>
       </Row>
--- src/components/NavBar/NavBarBot.js
+++ src/components/NavBar/NavBarBot.js
@@ -1,5 +1,4 @@
 import Navbar from "react-bootstrap/Navbar";
-import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
