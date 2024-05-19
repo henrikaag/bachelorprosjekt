ad2405d2d11af0db7b5aa0fed5c80877a8605de5
MarkusPra <Markus.praner@outlook.com>
2024-05-19 12:25:56 +0200

    ?

--- src/components/ForYou/KlimaAlt.js
+++ src/components/ForYou/KlimaAlt.js
@@ -5,21 +5,21 @@ function KlimaAlt() {
         <Container className="klima-alt-container">
             <Row>
             <h1>Klimavennlige alternativer</h1>
-            <p>Varer du ofte hanler, som har et mer klimavennlig alternativ</p>
+            <p>Varer du ofte handler, som har et mer klimavennlig alternativ</p>
             </Row>
             <Row className="klima-alt-row">
                 <Col className="px-1">
                     <Card className="vare">
-                        <Card.Img variant="top" src={require(`../../img/boil-in-bag.jpeg`)}>
+                        <Card.Img variant="top" src={require(`../../img/Indrefilet.jpg`)}>
                         </Card.Img>
                         <Card.Body className="vare-body">
                                 <Row>
-                                <Card.Title> Vare </Card.Title>
-                                <Card.Text>Info</Card.Text>
+                                <Card.Title>Indrefilet Svin </Card.Title>
+                                <Card.Text>renskjært, 600 g</Card.Text>
                                 </Row>
                                 <Row className="mt-4">
-                                <Card.Title>Pris</Card.Title>
-                                <Card.Text>Kilo</Card.Text>
+                                <Card.Title>139,00 kr</Card.Title>
+                                <Card.Text>231,67 kr/kg</Card.Text>
                                 </Row>
                                 <Button className="btn-warning">Bytt ut</Button>
                             </Card.Body>
@@ -27,16 +27,16 @@ function KlimaAlt() {
                 </Col>
                 <Col className="px-1">
                     <Card className="vare">
-                        <Card.Img variant="top" src={require(`../../img/boil-in-bag.jpeg`)}>
+                        <Card.Img variant="top" src={require(`../../img/kyllingfilet.jpg`)}>
                         </Card.Img>
                         <Card.Body className="vare-body">
                                 <Row>
-                                <Card.Title> Vare </Card.Title>
-                                <Card.Text>Info</Card.Text>
+                                <Card.Title> Kyllingfilet naturell </Card.Title>
+                                <Card.Text>kylling, 900 g</Card.Text>
                                 </Row>
                                 <Row className="mt-4">
-                                <Card.Title>Pris</Card.Title>
-                                <Card.Text>Kilo</Card.Text>
+                                <Card.Title>144,00 kr</Card.Title>
+                                <Card.Text>160,00 kr/kg</Card.Text>
                                 </Row>
                                 <Button className="btn-warning">Bytt ut</Button>
                             </Card.Body>
@@ -44,16 +44,16 @@ function KlimaAlt() {
                 </Col>
                 <Col className="px-1">
                     <Card className="vare">
-                        <Card.Img variant="top" src={require(`../../img/boil-in-bag.jpeg`)}>
+                        <Card.Img variant="top" src={require(`../../img/roastbiff.jpg`)}>
                         </Card.Img>
                         <Card.Body className="vare-body">
                                 <Row>
-                                <Card.Title> Vare </Card.Title>
-                                <Card.Text>Info</Card.Text>
+                                <Card.Title> Roastbeef </Card.Title>
+                                <Card.Text>100 g, REMA 1000</Card.Text>
                                 </Row>
                                 <Row className="mt-4">
-                                <Card.Title>Pris</Card.Title>
-                                <Card.Text>Kilo</Card.Text>
+                                <Card.Title>32,70 kr</Card.Title>
+                                <Card.Text>327,00 kr/kg</Card.Text>
                                 </Row>
                                 <Button className="btn-warning">Bytt ut</Button>
                             </Card.Body>
@@ -61,16 +61,16 @@ function KlimaAlt() {
                 </Col>
                 <Col className="px-1">
                     <Card className="vare">
-                        <Card.Img variant="top" src={require(`../../img/boil-in-bag.jpeg`)}>
+                        <Card.Img variant="top" src={require(`../../img/leverpostei.png`)}>
                         </Card.Img>
                         <Card.Body className="vare-body">
                                 <Row>
-                                <Card.Title> Vare </Card.Title>
-                                <Card.Text>Info</Card.Text>
+                                <Card.Title> Leverpostei </Card.Title>
+                                <Card.Text>100 g Stabburet</Card.Text>
                                 </Row>
                                 <Row className="mt-4">
-                                <Card.Title>Pris</Card.Title>
-                                <Card.Text>Kilo</Card.Text>
+                                <Card.Title>13,50 kr</Card.Title>
+                                <Card.Text>135,00 kr/kg</Card.Text>
                                 </Row>
                                 <Button className="btn-warning">Bytt ut</Button>
                             </Card.Body>
@@ -78,33 +78,16 @@ function KlimaAlt() {
                 </Col>
                 <Col className="px-1">
                     <Card className="vare">
-                        <Card.Img variant="top" src={require(`../../img/boil-in-bag.jpeg`)}>
+                        <Card.Img variant="top" src={require(`../../img/Norveiga.jpg`)}>
                         </Card.Img>
                         <Card.Body className="vare-body">
                                 <Row>
-                                <Card.Title> Vare </Card.Title>
-                                <Card.Text>Info</Card.Text>
+                                <Card.Title> Norveiga 26% </Card.Title>
+                                <Card.Text>Skorpefri 500 g</Card.Text>
                                 </Row>
                                 <Row className="mt-4">
-                                <Card.Title>Pris</Card.Title>
-                                <Card.Text>Kilo</Card.Text>
-                                </Row>
-                                <Button className="btn-warning">Bytt ut</Button>
-                            </Card.Body>
-                    </Card>
-                </Col>
-                <Col className="px-1">
-                    <Card className="vare">
-                        <Card.Img variant="top" src={require(`../../img/boil-in-bag.jpeg`)}>
-                        </Card.Img>
-                        <Card.Body className="vare-body">
-                                <Row>
-                                <Card.Title> Vare </Card.Title>
-                                <Card.Text>Info</Card.Text>
-                                </Row>
-                                <Row className="mt-4">
-                                <Card.Title>Pris</Card.Title>
-                                <Card.Text>Kilo</Card.Text>
+                                <Card.Title>81,00 kr</Card.Title>
+                                <Card.Text>162,00 kr/kg</Card.Text>
                                 </Row>
                                 <Button className="btn-warning">Bytt ut</Button>
                             </Card.Body>
--- src/components/ForYou/Modals/BilligModal.js
+++ src/components/ForYou/Modals/BilligModal.js
@@ -29,16 +29,16 @@ function BilligModal(props) {
             <Row className="billig-alt-row">
                 <Col className="px-1">
                     <Card className="vare">
-                        <Card.Img variant="top" src={require(`../../../img/boil-in-bag.jpeg`)}>
+                        <Card.Img variant="top" src={require(`../../../img/Cola4pk.jpg`)}>
                         </Card.Img>
                         <Card.Body className="vare-body">
                                 <Row>
-                                <Card.Title> Vare </Card.Title>
-                                <Card.Text>Info</Card.Text>
+                                <Card.Title> Coca-Cola Regular </Card.Title>
+                                <Card.Text>4 x 0,25 l</Card.Text>
                                 </Row>
                                 <Row className="mt-4">
-                                <Card.Title>Pris</Card.Title>
-                                <Card.Text>Kilo</Card.Text>
+                                <Card.Title>44,90 kr</Card.Title>
+                                <Card.Text>99,99 kr/l</Card.Text>
                                 </Row>
                                 <Button className="btn-warning">Bytt ut</Button>
                             </Card.Body>
@@ -46,16 +46,16 @@ function BilligModal(props) {
                 </Col>
                 <Col className="px-1">
                     <Card className="vare">
-                        <Card.Img variant="top" src={require(`../../../img/boil-in-bag.jpeg`)}>
+                        <Card.Img variant="top" src={require(`../../../img/Cola10pk.jpg`)}>
                         </Card.Img>
                         <Card.Body className="vare-body">
                                 <Row>
-                                <Card.Title> Vare </Card.Title>
-                                <Card.Text>Info</Card.Text>
+                                <Card.Title> Coca-Cola </Card.Title>
+                                <Card.Text>10 x 0,33 l</Card.Text>
                                 </Row>
                                 <Row className="mt-4">
-                                <Card.Title>Pris</Card.Title>
-                                <Card.Text>Kilo</Card.Text>
+                                <Card.Title>99,99 kr</Card.Title>
+                                <Card.Text>99,99 kr/l</Card.Text>
                                 </Row>
                                 <Button className="btn-warning">Bytt ut</Button>
                             </Card.Body>
@@ -63,54 +63,56 @@ function BilligModal(props) {
                 </Col>
                 <Col className="px-1">
                     <Card className="vare">
-                        <Card.Img variant="top" src={require(`../../../img/boil-in-bag.jpeg`)}>
+                        <Card.Img variant="top" src={require(`../../../img/Cola1.5.jpg`)}>
                         </Card.Img>
                         <Card.Body className="vare-body">
                                 <Row>
-                                <Card.Title> Vare </Card.Title>
-                                <Card.Text>Info</Card.Text>
+                                <Card.Title> Coca-Coca </Card.Title>
+                                <Card.Text>4 x 1,5 l</Card.Text>
                                 </Row>
                                 <Row className="mt-4">
-                                <Card.Title>Pris</Card.Title>
-                                <Card.Text>Kilo</Card.Text>
+                                <Card.Title>80,10 kr</Card.Title>
+                                <Card.Text>13,35 kr/l</Card.Text>
                                 </Row>
                                 <Button className="btn-warning">Bytt ut</Button>
                             </Card.Body>
                     </Card>
                 </Col>
                 <Col className="px-1">
+                </Col>
+                <Col className="px-1">
+                </Col>
+            </Row>
+            <Row>
+                <div className="partivare">
+                    <h4> Partivarer fra Oda XL </h4>
+                </div>
+            </Row>
+            <Row>
+            <Col className="px-1">
                     <Card className="vare">
-                        <Card.Img variant="top" src={require(`../../../img/boil-in-bag.jpeg`)}>
+                        <Card.Img variant="top" src={require(`../../../img/Pepsi.avif`)}>
                         </Card.Img>
                         <Card.Body className="vare-body">
                                 <Row>
-                                <Card.Title> Vare </Card.Title>
-                                <Card.Text>Info</Card.Text>
+                                <Card.Title> Pepsi Max Brett </Card.Title>
+                                <Card.Text>20 x 0,33 l</Card.Text>
                                 </Row>
                                 <Row className="mt-4">
-                                <Card.Title>Pris</Card.Title>
-                                <Card.Text>Kilo</Card.Text>
+                                <Card.Title>169,90 kr</Card.Title>
+                                <Card.Text>25,74 kr/l</Card.Text>
                                 </Row>
                                 <Button className="btn-warning">Bytt ut</Button>
                             </Card.Body>
                     </Card>
                 </Col>
                 <Col className="px-1">
-                    <Card className="vare">
-                        <Card.Img variant="top" src={require(`../../../img/boil-in-bag.jpeg`)}>
-                        </Card.Img>
-                        <Card.Body className="vare-body">
-                                <Row>
-                                <Card.Title> Vare </Card.Title>
-                                <Card.Text>Info</Card.Text>
-                                </Row>
-                                <Row className="mt-4">
-                                <Card.Title>Pris</Card.Title>
-                                <Card.Text>Kilo</Card.Text>
-                                </Row>
-                                <Button className="btn-warning">Bytt ut</Button>
-                            </Card.Body>
-                    </Card>
+                </Col>
+                <Col className="px-1">
+                </Col>
+                <Col className="px-1">
+                </Col>
+                <Col className="px-1">
                 </Col>
             </Row>
         </Container> 
